b74a25cd0cbed0e62b19728f498ed5d1b18437f9
AliMasud05 <morshedalimasud3@gmail.com>
2022-10-28 23:24:47 +0600

    final

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ const subject = require('./data/subject.json');
 const subjects = require('./data/subjects.json');
 
 
+
+app.get('/', (req, res) => {
+    res.send('subject run');
+});
+app.get('/subject', (req, res) => {
+    res.send(subject);
+});
+app.get('/subjects', (req, res) => {
+    res.send(subjects);
+})
+app.get('/subject/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedSubject = subjects.find(n => n.id === id);
+    res.send(selectedSubject);
+});
+
+
+app.listen(port, () => {
+    console.log('subject running', port);
+})
