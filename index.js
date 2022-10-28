bb6b31c8aebf56bc90663a36f3d4372c1872c2e4
AliMasud05 <morshedalimasud3@gmail.com>
2022-10-28 23:24:03 +0600

    var

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+const subject = require('./data/subject.json');
+const subjects = require('./data/subjects.json');
 
 
